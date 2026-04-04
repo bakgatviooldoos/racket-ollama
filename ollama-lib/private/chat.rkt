b09093bef740b265101c29f001a00514ec29b488
Christiaan <christiaan.brand@securicom.us.com>
2026-04-04 16:36:44 +0200

    chat: remove the parameter declarations from 'with-ollama-chat'

--- ollama-lib/private/chat.rkt
+++ ollama-lib/private/chat.rkt
@@ -3,19 +3,10 @@
 (provide
   with-ollama-chat)
 
-(define-syntax with-ollama-chat
-  (syntax-rules ()
-    [(_ [(more continue) start-chat]
-        #:declare ([arg init] ...)
-        . body)
-     (let ([arg (make-parameter #f)] ...)
-       (parameterize ([arg init] ...)
-         (with-ollama-chat [(more continue) start-chat]
-           . body)))]
-    
-    [(_ [(more continue) start-chat]
-        . body)
-     (let-values ([(more continue) start-chat])
-       (let loop ([more more] [continue continue])
-         (let ([continue (compose loop continue)])
-           . body)))]))
+(define-syntax-rule
+  (with-ollama-chat [(more continue) start-chat]
+    . body)
+  (let-values ([(more continue) start-chat])
+    (let loop ([more more] [continue continue])
+      (let ([continue (compose loop continue)])
+        . body))))
